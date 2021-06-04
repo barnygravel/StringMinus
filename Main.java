d4574bef8fd8e164e4fea2c041d388b268bad53f
barnygravel <>
2021-06-04 20:02:11 +0000

    Exported to Method

--- Main.java
+++ Main.java
@@ -5,6 +5,11 @@ class Main {
     List<String> list1 = Arrays.asList("abc","def","xyz");
     List<String> list2 = Arrays.asList("abc","def");
 
+    stringMinus(list1, list2);
+
+  }
+
+  public static void stringMinus(List<String> list1, List<String> list2) {
     List<String> differences = new ArrayList<>(list1);
     differences.removeAll(list2);
     
